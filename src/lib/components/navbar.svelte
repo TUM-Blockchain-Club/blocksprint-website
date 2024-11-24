18adced4cdb21eba312028ddd8385b1507bdd7cb
jis3r <jis3r@protonmail.com>
2024-11-24 23:39:41 +0100

    add: basic navbar

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<nav
+	class="mx-auto flex h-12 w-full items-center justify-between rounded-2xl border border-zinc-800 px-3 py-2"
+>
+	<a href="https://blocksprint.tum-blockchain.com">TUM BLOCKCHAIN CLUB</a>
+
+	<div class="flex h-8 gap-4">
+		<Button class="h-8" variant="secondary">Contact</Button>
+		<Button class="h-8">Partnership</Button>
+	</div>
+</nav>
