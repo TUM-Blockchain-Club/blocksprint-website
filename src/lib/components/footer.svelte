0130152ca630c62af4a058b3b489f0f954cf78f5
jis3r <jis3r@protonmail.com>
2024-12-01 18:59:35 +0100

    add: footer links

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -15,16 +15,49 @@
 		<div class="smflex order-1 grid grid-cols-2 gap-16 sm:order-2">
 			<div class="flex flex-col">
 				<p class="mb-8 text-sm text-foreground">Legal</p>
-				<a href="/" class="mb-4 text-sm text-muted-foreground hover:underline">Imprint</a>
-				<a href="/" class="mb-4 text-sm text-muted-foreground hover:underline">Privacy</a>
-				<a href="/" class="mb-4 text-sm text-muted-foreground hover:underline">Contact</a>
+				<a
+					href="https://www.tum-blockchain.com/imprint"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="mb-4 text-sm text-muted-foreground hover:underline">Imprint</a
+				>
+				<a
+					href="https://www.tum-blockchain.com/privacy-policy"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="mb-4 text-sm text-muted-foreground hover:underline">Privacy</a
+				>
+				<a
+					href="mailto:industry@tum-blockchain.com"
+					class="mb-4 text-sm text-muted-foreground hover:underline">Contact</a
+				>
 			</div>
 			<div class="flex flex-col">
 				<p class="mb-8 text-sm text-foreground">Connect</p>
-				<a href="/" class="mb-4 text-sm text-muted-foreground hover:underline">Twitter</a>
-				<a href="/" class="mb-4 text-sm text-muted-foreground hover:underline">LinkedIn</a>
-				<a href="/" class="mb-4 text-sm text-muted-foreground hover:underline">Instagram</a>
-				<a href="/" class="mb-4 text-sm text-muted-foreground hover:underline">Discord</a>
+				<a
+					href="https://x.com/tbc_munich"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="mb-4 text-sm text-muted-foreground hover:underline">Twitter</a
+				>
+				<a
+					href="https://www.linkedin.com/company/tum-blockchain-club/"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="mb-4 text-sm text-muted-foreground hover:underline">LinkedIn</a
+				>
+				<a
+					href="https://www.instagram.com/tumblockchain/"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="mb-4 text-sm text-muted-foreground hover:underline">Instagram</a
+				>
+				<a
+					href="https://discord.gg/kqwYF2cHb9"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="mb-4 text-sm text-muted-foreground hover:underline">Discord</a
+				>
 			</div>
 		</div>
 	</div>
