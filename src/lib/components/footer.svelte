80ac3c0c44eb5af6e3a25a93ce1df4349faeba48
jis3r <jis3r@protonmail.com>
2024-12-03 01:42:21 +0100

    add: implement conditional rendering for footer component after mount

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,68 +1,73 @@
 <script>
 	import fattbc from '$lib/img/fattbc.svg';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import { fade, blur } from 'svelte/transition';
+	import { onMount } from 'svelte';
+
+	let mounted = false;
+
+	onMount(() => {
+		mounted = true;
+	});
 </script>
 
-<footer
-	class="mx-auto mt-16 max-w-6xl p-3 sm:mt-28 md:mt-40"
-	in:blur={{ delay: 1000, duration: 300 }}
->
-	<img src={fattbc} alt="TUM Blockchain Club" class="mb-12 w-full md:mb-16" />
-	<div class="flex flex-col justify-between sm:flex-row md:pr-12">
-		<div class="order-2 flex flex-col justify-between sm:order-1">
-			<div></div>
-			<p class="mx-auto mt-4 text-xs text-muted-foreground sm:mx-0 sm:mt-0 sm:text-sm">
-				Copyright © TUM Blockchain Club
-			</p>
-		</div>
-		<div class="smflex order-1 grid grid-cols-2 gap-16 sm:order-2">
-			<div class="flex flex-col">
-				<p class="mb-8 text-sm text-foreground">Legal</p>
-				<a
-					href="https://www.tum-blockchain.com/imprint"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="mb-4 text-sm text-muted-foreground hover:underline">Imprint</a
-				>
-				<a
-					href="https://www.tum-blockchain.com/privacy-policy"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="mb-4 text-sm text-muted-foreground hover:underline">Privacy</a
-				>
-				<a
-					href="mailto:industry@tum-blockchain.com"
-					class="mb-4 text-sm text-muted-foreground hover:underline">Contact</a
-				>
+{#if mounted}
+	<footer class="mx-auto mt-16 max-w-6xl p-3 sm:mt-28 md:mt-40">
+		<img src={fattbc} alt="TUM Blockchain Club" class="mb-12 w-full md:mb-16" />
+		<div class="flex flex-col justify-between sm:flex-row md:pr-12">
+			<div class="order-2 flex flex-col justify-between sm:order-1">
+				<div></div>
+				<p class="mx-auto mt-4 text-xs text-muted-foreground sm:mx-0 sm:mt-0 sm:text-sm">
+					Copyright © TUM Blockchain Club
+				</p>
 			</div>
-			<div class="flex flex-col">
-				<p class="mb-8 text-sm text-foreground">Connect</p>
-				<a
-					href="https://x.com/tbc_munich"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="mb-4 text-sm text-muted-foreground hover:underline">Twitter</a
-				>
-				<a
-					href="https://www.linkedin.com/company/tum-blockchain-club/"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="mb-4 text-sm text-muted-foreground hover:underline">LinkedIn</a
-				>
-				<a
-					href="https://www.instagram.com/tumblockchain/"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="mb-4 text-sm text-muted-foreground hover:underline">Instagram</a
-				>
-				<a
-					href="https://discord.gg/kqwYF2cHb9"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="mb-4 text-sm text-muted-foreground hover:underline">Discord</a
-				>
+			<div class="smflex order-1 grid grid-cols-2 gap-16 sm:order-2">
+				<div class="flex flex-col">
+					<p class="mb-8 text-sm text-foreground">Legal</p>
+					<a
+						href="https://www.tum-blockchain.com/imprint"
+						target="_blank"
+						rel="noopener noreferrer"
+						class="mb-4 text-sm text-muted-foreground hover:underline">Imprint</a
+					>
+					<a
+						href="https://www.tum-blockchain.com/privacy-policy"
+						target="_blank"
+						rel="noopener noreferrer"
+						class="mb-4 text-sm text-muted-foreground hover:underline">Privacy</a
+					>
+					<a
+						href="mailto:industry@tum-blockchain.com"
+						class="mb-4 text-sm text-muted-foreground hover:underline">Contact</a
+					>
+				</div>
+				<div class="flex flex-col">
+					<p class="mb-8 text-sm text-foreground">Connect</p>
+					<a
+						href="https://x.com/tbc_munich"
+						target="_blank"
+						rel="noopener noreferrer"
+						class="mb-4 text-sm text-muted-foreground hover:underline">Twitter</a
+					>
+					<a
+						href="https://www.linkedin.com/company/tum-blockchain-club/"
+						target="_blank"
+						rel="noopener noreferrer"
+						class="mb-4 text-sm text-muted-foreground hover:underline">LinkedIn</a
+					>
+					<a
+						href="https://www.instagram.com/tumblockchain/"
+						target="_blank"
+						rel="noopener noreferrer"
+						class="mb-4 text-sm text-muted-foreground hover:underline">Instagram</a
+					>
+					<a
+						href="https://discord.gg/kqwYF2cHb9"
+						target="_blank"
+						rel="noopener noreferrer"
+						class="mb-4 text-sm text-muted-foreground hover:underline">Discord</a
+					>
+				</div>
 			</div>
 		</div>
-	</div>
-</footer>
+	</footer>
+{/if}
