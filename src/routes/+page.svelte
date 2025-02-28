0ed84eec1ce6defb2fa1efa7195d0ebe3a3f4063
jis3r <jis3r@protonmail.com>
2025-02-28 23:32:01 +0100

    add: application button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,24 @@
 	import Testimonials from '$lib/components/testimonials.svelte';
 
 	let mounted = false;
+	let gradientAngle = 0;
+
 	onMount(() => {
 		mounted = true;
+
+		// Set up the animation loop for the gradient rotation
+		const animateGradient = () => {
+			gradientAngle = (gradientAngle + 1) % 360;
+			requestAnimationFrame(animateGradient);
+		};
+
+		// Start the animation
+		const animation = requestAnimationFrame(animateGradient);
+
+		// Clean up on component unmount
+		return () => {
+			cancelAnimationFrame(animation);
+		};
 	});
 </script>
 
@@ -31,7 +47,7 @@
 			3-month projects with innovative companies, students tackle exciting, real-world challenges.
 		</p>
 		<div class="mx-auto mt-5 grid w-fit gap-4 sm:flex" in:blur={{ delay: 50, duration: 500 }}>
-			<Button href="https://apply.tum-blockchain.com/industry-partner">Become a partner</Button>
+			<!-- <Button href="https://apply.tum-blockchain.com/industry-partner">Become a partner</Button>
 			<Button
 				href="https://apply.tum-blockchain.com/talent-pool"
 				variant="ghost"
@@ -41,7 +57,23 @@
 					Join the Talentpool
 				</p>
 				<ChevronRight class="h-5 w-5" />
-			</Button>
+			</Button> -->
+
+			<div
+				class="border-gradient overflow-hidden rounded-md transition-transform duration-300"
+				style:--gradient-angle="{gradientAngle}deg"
+			>
+				<Button
+					href="https://apply.tum-blockchain.com/blocksprint"
+					variant="ghost"
+					class="relative z-10 flex gap-1 hover:bg-zinc-900"
+				>
+					<p class="bg-gradient-to-r from-zinc-50 to-zinc-400 bg-clip-text text-transparent">
+						Apply now!
+					</p>
+					<ChevronRight class="h-5 w-5" />
+				</Button>
+			</div>
 		</div>
 	</div>
 
@@ -133,3 +165,22 @@
 		</div>
 	</div>
 {/if}
+
+<style>
+	.border-gradient {
+		--c: #09090b;
+		--p: 10%;
+		--gradient-angle: 0deg;
+		background:
+			linear-gradient(var(--c), var(--c)) padding-box,
+			conic-gradient(
+					from var(--gradient-angle),
+					transparent,
+					white var(--p),
+					transparent calc(var(--p) * 2)
+				)
+				border-box;
+		border: 1px solid transparent;
+		position: relative;
+	}
+</style>
